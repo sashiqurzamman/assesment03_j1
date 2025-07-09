cd0b6d76ebd476fdfe03d90c81b24ed6b38dfd74
SM ASHIQURZAMMAN <ashiqurzamman.amad-05-01@dipti.com.bd>
2025-07-09 12:25:56 +0600

    almost ready

--- app/src/main/java/com/example/a3_j1_ashik/MainActivity.kt
+++ app/src/main/java/com/example/a3_j1_ashik/MainActivity.kt
@@ -3,21 +3,20 @@ package com.example.a3_j1_ashik
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.a3_j1_ashik.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val listView = binding.listView
+
         val personList = listOf(
             PersonDataClass(R.drawable.avatar, "Abir", "123 Main St, Springfield"),
             PersonDataClass(R.drawable.avatar2, "Nusrat Dana", "456 Elm St, Springfield"),
@@ -26,7 +25,8 @@ class MainActivity : AppCompatActivity() {
             PersonDataClass(R.drawable.woman, "Munia Mehrah", "789 Oak St, Springfield")
         )
 
-        val adapter =PersonAdapter(this, personList)
-        listView.adapter = adapter
+
+        val adapter = PersonAdapter(this, personList)
+        binding.listView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/a3_j1_ashik/PersonAdapter.kt
+++ app/src/main/java/com/example/a3_j1_ashik/PersonAdapter.kt
@@ -10,21 +10,23 @@ import de.hdodenhof.circleimageview.CircleImageView
 
 class PersonAdapter(context: Context, private val personList: List<PersonDataClass>) :
     ArrayAdapter<PersonDataClass>(context, 0, personList) {
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var itemView = convertView
         if (itemView == null) {
             itemView = LayoutInflater.from(context).inflate(R.layout.item_person, parent, false)
         }
 
-        val person =personList[position]
-        val imageViewPerson =itemView!!.findViewById<CircleImageView>(R.id.profile_image)
+        val person = personList[position]
+
+        val imageViewPerson = itemView!!.findViewById<CircleImageView>(R.id.profile_image)
         val nameView = itemView.findViewById<TextView>(R.id.nameTV)
         val addressView = itemView.findViewById<TextView>(R.id.addressTV)
 
         imageViewPerson.setImageResource(person.imgResId)
         nameView.text = person.name
         addressView.text = person.address
+
         return itemView
     }
-
 }
